dbc3e7dfbd5d15501d77365dec028e82d8cf9298
Adam Drummond <adam@Adams-MacBook-Pro.local>
2022-11-04 11:23:02 +0000

    added data from api, updated manifest to remove default react text

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Row } from "react-bootstrap";
 import "./Data.css";
 
@@ -5,9 +6,30 @@ export default function Data(){
 
     
 
+    const [data, setData] = useState({message:[
+        {"fields":{"Quote":"| - Loading","Who":"","Date":""}, "index":0},
+    ]});
+    
+    async function getDataFromDB(){
+        await fetch(`https://adamdrummond.api.stdlib.com/lq@dev/?data=true`)
+            .then((response) => response.json())
+            .then((data) => {
+                // data.message.map((e) => {
+                //     console.log(e);
+                // });
+                setData(data);
+            });
+
+    }
+
+getDataFromDB()
+
     return(
         <Row className="data">
             <h1>Data</h1>
+            {data.message.map((e) => (
+                <p key={e.index}>{e.fields.Quote} - {e.fields.Lecturer} | {e.fields.Date}</p>
+            ))}
         </Row>
     )
 }
